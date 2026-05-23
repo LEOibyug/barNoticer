c9a9d41ec5251606038d77b8fde3e15b26449a88
LEOibyug <y2695719556@outlook.com>
2026-05-23 14:39:20 +0800

    Initial Commit

---
+++ barNoticer/barNoticerApp.swift
@@ -0,0 +1,17 @@
+//
+//  barNoticerApp.swift
+//  barNoticer
+//
+//  Created by  Rhetoric on 2026/5/23.
+//
+
+import SwiftUI
+
+@main
+struct barNoticerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
