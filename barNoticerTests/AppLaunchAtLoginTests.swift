c60f1c5de4dc9aa1166dc87f6a89ce77186442bb
LEOibyug <y2695719556@outlook.com>
2026-05-24 22:56:23 +0800

    fix: hide app from dock

--- barNoticerTests/AppLaunchAtLoginTests.swift
+++ barNoticerTests/AppLaunchAtLoginTests.swift
@@ -3,6 +3,16 @@ import XCTest
 @testable import barNoticer
 
 final class AppLaunchAtLoginTests: XCTestCase {
+    func testGeneratedInfoPlistDeclaresAccessoryApp() throws {
+        let projectFile = URL(fileURLWithPath: #filePath)
+            .deletingLastPathComponent()
+            .deletingLastPathComponent()
+            .appendingPathComponent("barNoticer.xcodeproj/project.pbxproj")
+        let project = try String(contentsOf: projectFile, encoding: .utf8)
+
+        XCTAssertEqual(project.components(separatedBy: "INFOPLIST_KEY_LSUIElement = YES;").count - 1, 2)
+    }
+
     func testLaunchAtLoginStatusMapsEnabledStates() {
         XCTAssertTrue(AppLaunchAtLoginStatus(status: .enabled).isEnabled)
         XCTAssertTrue(AppLaunchAtLoginStatus(status: .requiresApproval).isEnabled)
